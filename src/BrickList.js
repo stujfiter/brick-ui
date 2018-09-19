0662fdb9261241fc0bb6cf64c51a20c7621b4268
Korey Earl <korey.earl@gmail.com>
2018-09-19 11:10:04 -0500

    Introduce card view with Image

--- src/BrickList.js
+++ src/BrickList.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
 
+class BrickItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={};
+  }
+
+  render() {
+    return (
+      <div className="brick-item">
+        <div className="brick-item-pic"><img src="https://i.imgur.com/0hHQDjFb.jpg" alt="2x2 Brick" /></div>
+        <div className="brick-item-part">{this.props.partNumber}</div>
+        <div className="brick-item-desc">{this.props.description}</div>
+      </div>
+    )
+  }
+}
+
 class BrickList extends Component {
     render() {
       const listItems = [];
   
       this.props.bricks.forEach((brick) => {
         listItems.push(
-          <tr key={brick.description}>
-          <td>{brick.partNumber}</td>
-          <td>{brick.description}</td>
-          </tr>
+          <BrickItem 
+            key={brick.partNumber} 
+            partNumber={brick.partNumber} 
+            description={brick.description} 
+          />
         );
       });
   
       return (
         <div className="brick-list">
-          <table>
-            <tr>
-              <th>Part Number</th>
-              <th>Description</th>
-            </tr>
-            {listItems}
-          </table>
+          {listItems}
         </div>
       );
     }
